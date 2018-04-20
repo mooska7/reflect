762b9dd6e66b5760dfdbe858a6cb32d1332be399
gegela <1367260592@qq.com>
2018-04-20 16:11:40 +0800

    文件提交

---
+++ src/main/java/com/klt/reflect/ArrayTester2.java
@@ -0,0 +1,22 @@
+package com.klt.reflect;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class ArrayTester2
+{
+	public static void main(String args[])
+	{
+		int[] dims = new int[] { 5, 10, 15 };
+		System.out.println(dims[1]);
+		Object array = Array.newInstance(Integer.TYPE, dims);
+		System.out.println(Arrays.toString(dims));
+		Object arrayObj = Array.get(array, 3);
+		Class<?> cls = arrayObj.getClass().getComponentType();
+		System.out.println(cls);
+		arrayObj = Array.get(arrayObj, 5);
+		Array.setInt(arrayObj, 10, 37);
+		int arrayCast[][][] = (int[][][]) array;
+		System.out.println(arrayCast[3][5][10]);
+	}
+}
--- src/main/java/com/klt/reflect/ClassDemo.java
+++ src/main/java/com/klt/reflect/ClassDemo.java
@@ -5,7 +5,7 @@ import java.lang.reflect.Method;
 public class ClassDemo {
 	public static void main(String[] args) {
 		try {
-			Class<?> classType = Class.forName(args[0]);
+			Class<?> classType = Class.forName(args[1]);
 			Method[] methods = classType.getDeclaredMethods();
 			for(Method method:methods){
 				System.out.println(method.toString());
